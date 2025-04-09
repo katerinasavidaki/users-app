7e971b4bc4c6458aaeb56b3645a85a148a218a39
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-09 19:24:41 +0300

    feat: google login created

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
 
 function generateAccessToken(user) {
   console.log("Auth Service", user);
@@ -24,6 +25,33 @@ function verifyAccessToken(token) {
   } catch (err) {
     return {verified: false, data: err.message};
   }
-}
+};
 
-module.exports = { generateAccessToken, verifyAccessToken }
+async function googleAuth(code) {
+  console.log("Google login");
+  const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+
+  const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+  try {
+    // Exchange code for tokens
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    const ticket = await oauth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+      audience: CLIENT_ID
+    });
+
+    const userInfo = await ticket.getPayLoad();
+    console.log("Google user", userInfo);
+    return {user: userInfo, tokens};
+  } catch (error) {
+    console.log("Error in google authentication", error);
+    return { error: "Failed to authenticate with google" };
+  }
+};
+
+module.exports = { generateAccessToken, verifyAccessToken, googleAuth }
