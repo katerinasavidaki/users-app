9c998f9a4c0759f92f80c1bb318d62b914fe0e80
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-16 15:03:10 +0300

    feat: jest tests for endpoints

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
+const userService = require('../services/user.services')
 
 const authService = require('../services/auth.service');
 const app = require('../app');
@@ -41,6 +42,86 @@ describe("Requests for /api/users", () => {
     expect(res.body.status).toBeTruthy();
     expect(res.body.data.length).toBeGreaterThan(0);
   }, 50000);
+
+  it("POST creates a user", async () => {
+    const res = await request(app)
+    .post('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+      "username": "test5",
+      "password": "12345",
+      "name": "test5 name",
+      "surname": "test5 surname",
+      "email": "test5@aueb.gr",
+      "address": {
+        "area": "area1",
+        "road": "road1"
+      }
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBeTruthy();
+  }, 50000)
+
+  it("POST creates a user that exists", async () => {
+    const res = await request(app)
+    .post('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    send({
+      "username": "test5",
+      "password": "12345",
+      "name": "new name",
+      "surname": "new surname",
+      "email": "new@aueb.gr",
+      "adress": {
+        "area": "area1",
+        "road": "road2"
+      }
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.status).not.toBeTruthy();
+  }, 50000);
+
+  it("Post creates a user with same email", async () => {
+    const res = await request(app)
+    .post('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+      'username': "test6",
+      "password": "12345",
+      "name": "test6",
+      "surname": "surname6",
+      'email': "test5@aueb.gr",
+      "address": {
+        "road": "road1",
+        "area": "area1"
+      }
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.status).not.toBeTruthy();
+  }, 50000);
+
+  it('POST creates a user with empty name, surname, password', async () => {
+    const res = await request(app)
+    .post('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+      "username": "",
+      "password": "",
+      "name": "",
+      "surname": "lalala",
+      "email": "testempty@aueb.gr",
+      "address": {
+        "area": "area1",
+        "road": "road2"
+      }
+    });
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.status).not.toBeTruthy();
+  }, 50000)
 });
 
 describe("Requests for /api/users/:username", () => {
@@ -56,8 +137,11 @@ describe("Requests for /api/users/:username", () => {
   });
 
   it('GET returns specific user', async () => {
+
+    const result = await userService.findLastInsertedUser();
+
     const res = await request(app)
-    .get('/api/users/user1')
+    .get('/api/users/' + result.username)
     .set('Authorization', `Bearer ${token}`);
 
     expect(res.statusCode).toBe(200);
